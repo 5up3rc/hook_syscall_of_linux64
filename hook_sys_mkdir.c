7bd345c801dc1366b4df2901f1203084e4544842
azure <fyppyyxx@126.com>
2017-07-16 22:02:48 +0800

    hook sys_mkdir under linux64

---
+++ hook_sys_mkdir.c
@@ -0,0 +1,108 @@
+/*
+* This module need to be compile in 64-bit using GCC.
+*/
+#include <linux/module.h> /* module_init module_exit */
+#include <linux/init.h> /* __init __exit */
+#include <linux/syscalls.h> /* sys_close __NR_close __NR_mkdir */
+#include <linux/delay.h> /* loops_per_jiffy */
+#include <asm/bitops.h> /* set_bit clear_bit */
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("sdingcn@gmail.com");
+MODULE_DESCRIPTION("This module can hook the sys_mkdir.");
+
+/*
+* These global variables are used to store the virtual address of the sys_call_table
+* and the original virtual address of the sys_mkdir.
+*/
+static unsigned long **sys_call_table;
+static asmlinkage long (*original_sys_mkdir)(const char __user *pathname, umode_t mode);
+
+/* These tool functions are used to enable writing on the sys_call_table. */
+static void disable_write_protection(void);
+static void enable_write_protection(void);
+
+static unsigned long **find_sys_call_table(void);
+
+/* We need to define a new sys_mkdir, and use the new function to replace the original function. */
+static asmlinkage long new_sys_mkdir(const char __user *pathname, umode_t mode);
+
+static int __init hook_sys_mkdir_init(void);
+static void __exit hook_sys_mkdir_exit(void);
+
+module_init(hook_sys_mkdir_init);
+module_exit(hook_sys_mkdir_exit);
+
+static void disable_write_protection(void)
+{
+	unsigned long cr0 = read_cr0();
+	clear_bit(16, &cr0);
+	write_cr0(cr0);
+}
+
+static void enable_write_protection(void)
+{
+	unsigned long cr0 = read_cr0();
+	set_bit(16, &cr0);
+	write_cr0(cr0);
+}
+
+static unsigned long **find_sys_call_table(void)
+{
+	/* use long to represent the virtual address */
+	unsigned long vaddr_number;
+	
+	/* We want this pointer to point to the sys_call_table. */
+	unsigned long **p;
+
+	/*
+	* It seems that the virtual address of the sys_call_table is always between
+	* the virtual address of the sys_close and the virtual address of the loops_per_jiffy,
+	* so we can search this area to find the sys_call_table.
+	*/
+	for (vaddr_number = (unsigned long)sys_close;
+		vaddr_number < (unsigned long)&loops_per_jiffy;
+	vaddr_number += sizeof(void *)) {
+		p = (unsigned long **)vaddr_number;
+		if (p[__NR_close] == (unsigned long *)sys_close)
+			return p;
+	}
+
+	/* If this function cannot find the virtual address of sys_call_table, it will return NULL. */
+	return NULL;
+}
+
+static asmlinkage long new_sys_mkdir(const char __user *pathname, umode_t mode)
+{
+	/* When someone calls the sys_mkdir, this function will report it. */
+	printk("<0>""calling sys_mkdir --hook_sys_mkdir\n");
+	
+	/* We still need to pass the parameters to the original sys_mkdir to complete the procedure of the system call. */
+	return original_sys_mkdir(pathname, mode);
+}
+
+static int __init hook_sys_mkdir_init(void)
+{
+	sys_call_table = find_sys_call_table();
+
+	if (!sys_call_table) {
+		printk("<0>""The hook_sys_mkdir cannot find the sys_call_table.\n");
+		return -1;
+	}
+
+	/* save the original function's virtual address and modify the sys_call_table to replace the function */
+	original_sys_mkdir = sys_call_table[__NR_mkdir];
+	disable_write_protection();
+	sys_call_table[__NR_mkdir] = new_sys_mkdir;
+	enable_write_protection();
+
+	return 0;
+}
+
+static void __exit hook_sys_mkdir_exit(void)
+{
+	/* When this module exits, it will restore the original sys_call_table. */
+	disable_write_protection();
+	sys_call_table[__NR_mkdir] = original_sys_mkdir;
+	enable_write_protection();
+}
